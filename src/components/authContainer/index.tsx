56d237ef52bd1745ee56057de8bb5d57d82f76cb
Dwynr <dwynr@outlook.de>
2024-08-21 03:36:40 +0200

    feat: add topbar nav to auth container on windows and linux

--- src/components/authContainer/index.tsx
+++ src/components/authContainer/index.tsx
@@ -1,12 +1,58 @@
-import { memo } from "react"
+import { memo, useCallback } from "react"
 import { Link } from "@tanstack/react-router"
 import { useTranslation } from "react-i18next"
 import { useTheme } from "@/providers/themeProvider"
 import LogoSVG from "@/assets/logo"
+import { cn } from "@/lib/utils"
+import { showConfirmDialog } from "../dialogs/confirm"
+import { Minus, Maximize, X } from "lucide-react"
+import { DESKTOP_TOPBAR_HEIGHT, IS_APPLE_DEVICE, IS_DESKTOP } from "@/constants"
 
 export const AuthContainer = memo(({ children }: { children: React.ReactNode }) => {
 	const { t } = useTranslation()
 	const { dark } = useTheme()
+
+	const minimizeWindow = useCallback(async () => {
+		try {
+			await window.desktopAPI.minimizeWindow()
+		} catch (e) {
+			console.error(e)
+		}
+	}, [])
+
+	const maximizeWindow = useCallback(async () => {
+		try {
+			if (await window.desktopAPI.isWindowMaximized()) {
+				await window.desktopAPI.unmaximizeWindow()
+
+				return
+			}
+
+			await window.desktopAPI.maximizeWindow()
+		} catch (e) {
+			console.error(e)
+		}
+	}, [])
+
+	const closeWindow = useCallback(
+		async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+			if (!e.shiftKey) {
+				if (
+					!(await showConfirmDialog({
+						title: t("desktop.dialogs.close.title"),
+						continueButtonText: t("desktop.dialogs.close.continue"),
+						description: t("desktop.dialogs.close.description"),
+						continueButtonVariant: "destructive"
+					}))
+				) {
+					return
+				}
+			}
+
+			window.desktopAPI.closeWindow().catch(console.error)
+		},
+		[t]
+	)
 
 	return (
 		<div className="flex h-[100dvh] w-screen flex-col overflow-y-auto overflow-x-hidden">
@@ -17,6 +63,53 @@ export const AuthContainer = memo(({ children }: { children: React.ReactNode }) 
 					WebkitAppRegion: "drag"
 				}}
 			>
+				{IS_DESKTOP && !IS_APPLE_DEVICE && (
+					<div
+						className="flex flex-row absolute right-0 top-0 z-50"
+						style={{
+							// @ts-expect-error not typed
+							WebkitAppRegion: "no-drag",
+							height: DESKTOP_TOPBAR_HEIGHT
+						}}
+					>
+						<div
+							className={cn(
+								"w-auto px-2 z-10 cursor-pointer text-muted-foreground h-full flex flex-row items-center justify-center hover:text-primary",
+								dark ? "hover:bg-secondary" : "hover:bg-[lightgray]"
+							)}
+							style={{
+								// @ts-expect-error not typed
+								WebkitAppRegion: "no-drag"
+							}}
+							onClick={minimizeWindow}
+						>
+							<Minus size={15} />
+						</div>
+						<div
+							className={cn(
+								"w-auto px-2 z-10 cursor-pointer text-muted-foreground h-full flex flex-row items-center justify-center hover:text-primary",
+								dark ? "hover:bg-secondary" : "hover:bg-[lightgray]"
+							)}
+							style={{
+								// @ts-expect-error not typed
+								WebkitAppRegion: "no-drag"
+							}}
+							onClick={maximizeWindow}
+						>
+							<Maximize size={13} />
+						</div>
+						<div
+							className="w-auto px-2 z-10 cursor-pointer text-muted-foreground h-full flex flex-row items-center justify-center hover:bg-red-600 hover:text-white"
+							style={{
+								// @ts-expect-error not typed
+								WebkitAppRegion: "no-drag"
+							}}
+							onClick={closeWindow}
+						>
+							<X size={15} />
+						</div>
+					</div>
+				)}
 				<Link
 					to="/login"
 					className="flex shrink-0 flex-row justify-center items-center gap-2"
