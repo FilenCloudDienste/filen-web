a6f634483e2ba8995eeff9df45fc14ba04c0782a
Julian Lagerberg <juliandreas@fastmail.com>
2024-11-18 16:45:44 +0100

    hide cancel button if lifetime plan

--- src/components/settings/subscriptions/index.tsx
+++ src/components/settings/subscriptions/index.tsx
@@ -119,12 +119,14 @@ export const Subscriptions = memo(() => {
 													? "Stripe"
 													: "Crypto"}
 										</p>
-										<p
-											className="text-sm underline mt-3 cursor-pointer"
-											onClick={() => cancel(subscription.id)}
-										>
-											{t("settings.subscriptions.cancel")}
-										</p>
+										{!subscription.planName.toLowerCase().includes("lifetime") && (
+											<p
+												className="text-sm underline mt-3 cursor-pointer"
+												onClick={() => cancel(subscription.id)}
+											>
+												{t("settings.subscriptions.cancel")}
+											</p>
+										)}
 									</div>
 								</div>
 							</div>
