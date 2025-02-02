ff52b2e3ff56f52ef443ddb74bf5777ca8a1444e
Dwynr <dwynr@outlook.de>
2025-02-02 23:23:12 +0100

    fix: pdf viewer password input

--- src/components/dialogs/previewDialog/pdf.tsx
+++ src/components/dialogs/previewDialog/pdf.tsx
@@ -5,6 +5,10 @@ import { cn } from "@/lib/utils"
 import useWindowSize from "@/hooks/useWindowSize"
 import useIsMobile from "@/hooks/useIsMobile"
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker?url"
+import { showInputDialog } from "../input"
+import { useTranslation } from "react-i18next"
+import { Button } from "@/components/ui/button"
+import { type OnItemClickArgs } from "react-pdf/dist/esm/shared/types.js"
 import "react-pdf/dist/Page/AnnotationLayer.css"
 import "react-pdf/dist/Page/TextLayer.css"
 
@@ -13,9 +17,11 @@ pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker
 export const PDF = memo(({ urlObject }: { urlObject: string }) => {
 	const [numPages, setNumPages] = useState<number>(0)
 	const [pageNumber, setPageNumber] = useState<number>(1)
-	const [hovering, setHovering] = useState<boolean>(false)
+	const [hovering, setHovering] = useState<boolean>(true)
 	const isMobile = useIsMobile()
 	const windowSize = useWindowSize()
+	const { t } = useTranslation()
+	const [didEnterNoPassword, setDidEnterNoPassword] = useState<boolean>(false)
 
 	const onLoadSuccess = useCallback(({ numPages }: { numPages: number }) => {
 		setNumPages(numPages)
@@ -101,6 +107,31 @@ export const PDF = memo(({ urlObject }: { urlObject: string }) => {
 		}
 	}, [])
 
+	const onPassword = useCallback(
+		async (callback: (password: string) => void) => {
+			const inputResponse = await showInputDialog({
+				title: t("dialogs.pdfPassword.title"),
+				continueButtonText: t("dialogs.pdfPassword.continue"),
+				value: "",
+				autoFocusInput: true,
+				placeholder: t("dialogs.pdfPassword.placeholder")
+			})
+
+			if (inputResponse.cancelled || inputResponse.value.length === 0) {
+				setDidEnterNoPassword(true)
+
+				return
+			}
+
+			callback(inputResponse.value)
+		},
+		[t]
+	)
+
+	const onItemClick = useCallback((args: OnItemClickArgs) => {
+		setPageNumber(args.pageNumber)
+	}, [])
+
 	const width = useMemo(() => {
 		return isMobile ? windowSize.width : 800
 	}, [isMobile, windowSize.width])
@@ -108,60 +139,79 @@ export const PDF = memo(({ urlObject }: { urlObject: string }) => {
 	return (
 		<div className="w-full h-full bg-primary-foreground">
 			<div className="flex flex-row w-full h-full justify-center">
-				<Document
-					file={urlObject}
-					onLoadSuccess={onLoadSuccess}
-					className={cn("flex flex-row h-full justify-center overflow-x-hidden overflow-y-auto select-text", `w-[${width}px]`)}
-					externalLinkTarget="_blank"
-					onMouseEnter={onMouseEnter}
-					onMouseLeave={onMouseLeave}
-					loading={components.loading}
-					error={components.error}
-					noData={components.noData}
-					options={
-						import.meta.env.DEV
-							? undefined
-							: {
-									standardFontDataUrl: "/pdfjs/standard_fonts/",
-									cMapUrl: "/pdfjs/cmaps/"
-								}
-					}
-				>
-					<Page
-						className="h-full select-text"
-						pageNumber={pageNumber}
-						loading={components.loading}
-						error={components.error}
-						noData={components.noData}
-						width={width}
-						scale={1}
-						canvasBackground="white"
-					/>
-				</Document>
-				<div
-					className={cn(
-						(isMobile || hovering) && numPages > 1 ? "opacity-100" : "opacity-0",
-						"flex flex-row items-center absolute bottom-4 left-1/2 -translate-x-1/2 bg-muted border rounded-md animate-in animate-out transition-all z-50 p-2 text-muted-foreground"
-					)}
-					onMouseEnter={onMouseEnter}
-					onMouseLeave={onMouseLeave}
-				>
-					<div
-						className="flex flex-row items-center hover:bg-muted/40 cursor-pointer hover:text-primary"
-						onClick={prevPage}
-					>
-						<ChevronLeft size={20} />
+				{didEnterNoPassword ? (
+					<div className="flex flex-col items-center justify-center w-full h-full">
+						<p>{t("previewDialog.pdfNoPassword")}</p>
+						<Button
+							className="mt-4"
+							onClick={() => setDidEnterNoPassword(false)}
+						>
+							{t("previewDialog.unlock")}
+						</Button>
 					</div>
-					<p className="flex flex-row items-center justify-center w-14">
-						{pageNumber} / {numPages}
-					</p>
-					<div
-						className="flex flex-row items-center hover:bg-muted/40 cursor-pointer hover:text-primary"
-						onClick={nextPage}
-					>
-						<ChevronRight size={20} />
-					</div>
-				</div>
+				) : (
+					<>
+						<Document
+							file={urlObject}
+							onLoadSuccess={onLoadSuccess}
+							className={cn(
+								"flex flex-row h-full justify-center overflow-x-hidden overflow-y-auto select-text",
+								`w-[${width}px]`
+							)}
+							externalLinkTarget="_blank"
+							onMouseEnter={onMouseEnter}
+							onMouseLeave={onMouseLeave}
+							loading={components.loading}
+							error={components.error}
+							noData={components.noData}
+							onPassword={onPassword}
+							onItemClick={onItemClick}
+							options={
+								import.meta.env.DEV
+									? undefined
+									: {
+											standardFontDataUrl: "/pdfjs/standard_fonts/",
+											cMapUrl: "/pdfjs/cmaps/"
+										}
+							}
+						>
+							<Page
+								className="h-full select-text"
+								pageNumber={pageNumber}
+								loading={components.loading}
+								error={components.error}
+								noData={components.noData}
+								width={width}
+								scale={1}
+								canvasBackground="white"
+							/>
+						</Document>
+						<div
+							className={cn(
+								(isMobile || hovering) && numPages > 1 ? "opacity-100" : "opacity-0",
+								"flex flex-row items-center absolute bottom-4 left-1/2 -translate-x-1/2 bg-muted border rounded-md animate-in animate-out transition-all z-50 p-2 text-muted-foreground"
+							)}
+							onMouseEnter={onMouseEnter}
+							onMouseLeave={onMouseLeave}
+						>
+							<div
+								className="flex flex-row items-center hover:bg-muted/40 cursor-pointer hover:text-primary"
+								onClick={prevPage}
+							>
+								<ChevronLeft size={20} />
+							</div>
+							<p className="flex flex-row items-center justify-center w-14">
+								{pageNumber} / {numPages}
+							</p>
+							<div
+								className="flex flex-row items-center hover:bg-muted/40 cursor-pointer hover:text-primary"
+								onClick={nextPage}
+							>
+								<ChevronRight size={20} />
+							</div>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	)
